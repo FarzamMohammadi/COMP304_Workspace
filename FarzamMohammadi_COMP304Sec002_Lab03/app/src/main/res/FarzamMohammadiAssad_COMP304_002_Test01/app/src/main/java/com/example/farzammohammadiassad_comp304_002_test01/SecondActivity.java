1404c0deed1945f9ff4fe92640a0d00b5d80a29f
FarzamMohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2021-03-03 21:35:46 -0500

    Midterm Assignment - 1st Activity - Complete

---
+++ FarzamMohammadi_COMP304Sec002_Lab03/app/src/main/res/FarzamMohammadiAssad_COMP304_002_Test01/app/src/main/java/com/example/farzammohammadiassad_comp304_002_test01/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.farzammohammadiassad_comp304_002_test01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
